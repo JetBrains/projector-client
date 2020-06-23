65d71c6faf917df92e6ee397677ea32ef20965b1
Ivan Seleznev <horis.iseleznev@gmail.com>
2020-06-23 14:14:26 +0300

    Fixed window header height and position for ideaServerWithAgent

--- projector-common/src/commonMain/kotlin/org/jetbrains/projector/common/protocol/toClient/WindowData.kt
+++ projector-common/src/commonMain/kotlin/org/jetbrains/projector/common/protocol/toClient/WindowData.kt
@@ -66,5 +66,17 @@ data class WindowData(
   @SerialName("j")
   val undecorated: Boolean,
   @SerialName("k")
-  val windowType: WindowType
+  val windowType: WindowType,
+  /**
+   * If the window has a header on the host, its sizes are included in the window bounds.
+   * The client header is drawn above the window, outside its bounds. At the same time,
+   * the coordinates of the contents of the window come taking into account the size
+   * of the header. As a result, on client an empty space is obtained between header
+   * and the contents of the window. To get rid of this, we transfer the height of the system
+   * window header and if it > 0, we draw the heading not over the window but inside
+   * the window's bounds, filling in the empty space.
+   * If the window has not a header on the host, headerHeight == null.
+   */
+  @SerialName("l")
+  val headerHeight: Int? = null
 )
