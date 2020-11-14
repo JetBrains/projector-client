82f2811dad265a64cf8669fd4452757954c4fdc1
SerVB <servbul@yandex.ru>
2020-11-14 23:32:40 +0300

    PRJ-169 Fix copying for win artifact

--- projector-launcher/build.gradle.kts
+++ projector-launcher/build.gradle.kts
@@ -15,15 +15,32 @@ val npmCommand = when (DefaultNativePlatform.getCurrentOperatingSystem().isWindo
     false -> listOf("npm")
 }
 
-val distElectron = task<Exec>("distElectron") {
+val distElectron by tasks.creating(Exec::class) {
+    group = "dist"
     workingDir(project.projectDir)
     commandLine(npmCommand + listOf("run", "packager"))
 }
 
-val dist = task<Exec>("dist") {
+val copyDeps by tasks.creating(Copy::class) {
+    group = "dist"
     dependsOn(distElectron)
-    workingDir(project.projectDir)
-    listOf("copy-deps-mac", "copy-deps-win", "copy-deps-lin").forEach {
-        commandLine(npmCommand + listOf("run", it))
+
+    // inspired by https://stackoverflow.com/a/42133267/6639500
+    val filesToCopy = copySpec {
+        from("build/js/node_modules")
     }
+
+    into("out")
+
+    val destinations = listOf(
+            "projector-linux-x64/resources/app/node_modules",
+            "projector-win32-x64/resources/app/node_modules",
+            "projector-darwin-x64/projector.app/Contents/Resources/app/node_modules"
+    )
+    destinations.forEach { into(it) { with(filesToCopy) } }
+}
+
+tasks.create("dist") {
+    group = "dist"
+    dependsOn(copyDeps)
 }
