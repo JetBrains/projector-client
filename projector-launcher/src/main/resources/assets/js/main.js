9920b7d5026f4f1c143ae9a9a98631bdce5e01b5
vicky17d <vicky17d@gmail.com>
2020-11-23 15:47:53 -0800

    Cache URLs in localStorage

--- projector-launcher/src/main/resources/assets/js/main.js
+++ projector-launcher/src/main/resources/assets/js/main.js
@@ -21,6 +21,26 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
+window.onload = function() {
+  document.getElementById("url-text-field").focus();
+};
+
+// Cache used URLs
+const storage = window.localStorage;
+const datalist = document.getElementById("url-data-list");
+const projectorLauncherStorageKey = "projector-launcher-urls"
+const results = (storage.getItem(projectorLauncherStorageKey)) ? JSON.parse(storage.getItem(projectorLauncherStorageKey)) : [];
+
+// Add cached URLs to the combobox datalist at start up
+results.forEach(result => addDataListOption(result))
+
+function addDataListOption(url) {
+  let option = document.createElement('option');
+  option.setAttribute('value', url);
+  option.innerText = url;
+  datalist.appendChild(option);
+}
+
 document.querySelector('#connect-button').addEventListener('click', function () {
   connect()
 });
@@ -33,8 +53,22 @@ document.querySelector('#url-text-field').addEventListener('keypress', function 
 
 function connect() {
   let url = document.getElementById("url-text-field").value;
+  if (isEmpty(url)) {
+    return;
+  }
+
   const {ipcRenderer} = require('electron')
   ipcRenderer.send("projector-connect", url);
+
+  cacheNewUrlValue(url);
+}
+
+function cacheNewUrlValue(url) {
+  if (results.indexOf(url) < 0) {
+    results.push(url);
+    storage.setItem(projectorLauncherStorageKey, JSON.stringify(results));
+    addDataListOption(url)
+  }
 }
 
 //$( document ).ready(function() {
@@ -44,3 +78,7 @@ ipcRenderer.on('projector-set-url', (event, arg) => {
   document.getElementById("url-text-field").value = arg
 })
 //});
+
+function isEmpty(str) {
+  return (!str || 0 === str.length);
+}
