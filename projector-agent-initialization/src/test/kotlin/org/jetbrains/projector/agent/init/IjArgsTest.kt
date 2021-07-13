80b959a2e59bcf6680061c88759d2c53cf1a6481
Art Pinch <leonardo906@mail.ru>
2021-07-13 13:59:53 +0300

    Add tests of agent arguments conversion

---
+++ projector-agent-initialization/src/test/kotlin/org/jetbrains/projector/agent/init/IjArgsTest.kt
@@ -0,0 +1,67 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019-2021 JetBrains s.r.o.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.jetbrains.projector.agent.init
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class IjArgsTest {
+
+  @Test
+  fun mapToStringTest() {
+
+    val obj = object : Any() {
+      override fun toString(): String {
+        return "customObject"
+      }
+    }
+
+    val map = mapOf(
+      "key1" to "stringValue",
+      "key2" to 2,
+      "key3" to 3.14,
+      "key4" to false,
+      "key5" to obj,
+    )
+
+    val argString = map.toIjArgs()
+
+    assertEquals("key1=stringValue;key2=2;key3=3.14;key4=false;key5=customObject", argString)
+  }
+
+  @Test
+  fun stringToMapTest() {
+
+    val argString = "key1=stringValue;key2=2;key3=3.14;key4=false;key5=customObject"
+
+    val map = argString.toArgsMap()
+
+    assertEquals(5, map.size)
+    assertEquals("stringValue", map["key1"])
+    assertEquals("2", map["key2"])
+    assertEquals("3.14", map["key3"])
+    assertEquals("false", map["key4"])
+    assertEquals("customObject", map["key5"])
+  }
+}
