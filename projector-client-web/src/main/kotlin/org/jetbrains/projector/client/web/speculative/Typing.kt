0ed309b804851bf80f37bd412946bc4b30c4f396
Art Pinch <leonardo906@mail.ru>
2021-07-15 11:00:44 +0300

    Skip speculative rendering of potentially invisible inputted chars and chars inputted with Ctrl/Alt/Meta modifiers

--- projector-client-web/src/main/kotlin/org/jetbrains/projector/client/web/speculative/Typing.kt
+++ projector-client-web/src/main/kotlin/org/jetbrains/projector/client/web/speculative/Typing.kt
@@ -27,6 +27,8 @@ import kotlinx.browser.document
 import org.jetbrains.projector.client.common.canvas.Extensions.toFontFaceName
 import org.jetbrains.projector.client.common.misc.ParamsProvider.SCALING_RATIO
 import org.jetbrains.projector.common.protocol.toClient.ServerCaretInfoChangedEvent
+import org.jetbrains.projector.common.protocol.toServer.ClientKeyPressEvent
+import org.jetbrains.projector.common.protocol.toServer.KeyModifier
 import org.w3c.dom.CanvasRenderingContext2D
 import org.w3c.dom.HTMLCanvasElement
 
@@ -36,7 +38,7 @@ sealed class Typing {
 
   abstract fun removeSpeculativeImage()
 
-  abstract fun addChar(char: Char)
+  abstract fun addEventChar(event: ClientKeyPressEvent)
 
   abstract fun dispose()
 
@@ -50,7 +52,7 @@ sealed class Typing {
       // do nothing
     }
 
-    override fun addChar(char: Char) {
+    override fun addEventChar(event: ClientKeyPressEvent) {
       // do nothing
     }
 
@@ -107,7 +109,23 @@ sealed class Typing {
       updateCanvas()
     }
 
-    override fun addChar(char: Char) {
+    /**
+     * Skip drawing speculative symbol when we cannot be sure char will be actually drawn on the server.
+     * Potentially invisible inputted chars:
+     *
+     * - Chars inputted with any of Ctrl/Alt/Meta modifiers (Example: Ctrl+C)
+     * - Chars with Unicode category from "Other" group (Example: Esc or Meta)
+     */
+    private fun shouldSkipEvent(event: ClientKeyPressEvent): Boolean {
+      return KeyModifier.CTRL_KEY in event.modifiers
+             || KeyModifier.ALT_KEY in event.modifiers
+             || KeyModifier.META_KEY in event.modifiers
+             || event.char.category.fromOtherUnicodeGroup
+    }
+
+    override fun addEventChar(event: ClientKeyPressEvent) {
+      if (shouldSkipEvent(event)) return
+
       val currentCarets = carets as? ServerCaretInfoChangedEvent.CaretInfoChange.Carets ?: return
 
       val canvas = canvasByIdGetter(currentCarets.editorWindowId) ?: return
@@ -148,7 +166,7 @@ sealed class Typing {
         fillStyle = "#888"  // todo: use a proper color
 
         val stringYPos = firstCaretLocation.y + currentCarets.fontSize  // todo: offsetting by fontSize seems to be not exact
-        fillText(char.toString(), firstCaretLocation.x, stringYPos)
+        fillText(event.char.toString(), firstCaretLocation.x, stringYPos)
       }
 
       speculativeCanvasImage.style.display = "block"
