f24d027f7a946c69194dc081769fd9c08ef048ed
avvessalom <avvessalom@gmail.com>
2021-07-20 11:14:28 +0300

    add coverage for client-common

--- projector-client-common/build.gradle.kts
+++ projector-client-common/build.gradle.kts
@@ -24,9 +24,39 @@
 plugins {
   kotlin("multiplatform")
   `maven-publish`
+  jacoco
+  java
+}
+
+jacoco {
+  toolVersion = "0.8.7"
 }
 
 val kotlinVersion: String by project
+
+tasks.withType<JacocoReport> {
+  dependsOn("jvmTest")
+  group = "Reporting"
+  description = "Generate Jacoco coverage reports"
+  val coverageSourceDirs = arrayOf(
+    "commonMain/src",
+    "jvmMain/src"
+  )
+  val classFiles = File("${buildDir}/classes/kotlin/jvm/")
+    .walkBottomUp()
+    .toSet()
+  classDirectories.setFrom(classFiles)
+  sourceDirectories.setFrom(files(coverageSourceDirs))
+  additionalSourceDirs.setFrom(files(coverageSourceDirs))
+  executionData
+    .setFrom(files("${buildDir}/jacoco/jvmTest.exec"))
+  reports {
+    xml.isEnabled = true
+    xml.destination = file(layout.buildDirectory.dir("../../JacocoReports/jacocoReportClientCommon.xml"))
+    csv.required.set(false)
+    html.outputLocation.set(layout.buildDirectory.dir("jacocoHtmlProjectorClient"))
+  }
+}
 
 kotlin {
   js {
