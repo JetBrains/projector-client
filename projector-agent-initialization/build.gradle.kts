e6d1af2dff6d09755498221059e21ee3e3fa5ba4
avvessalom <avvessalom@gmail.com>
2021-07-21 17:43:18 +0300

    add projector-client-agent-initialization publishing

--- projector-agent-initialization/build.gradle.kts
+++ projector-agent-initialization/build.gradle.kts
@@ -7,10 +7,25 @@ kotlin {
   explicitApi()
 }
 
+val usernameProp: String by project
+val passwordProp: String by project
+
 publishing {
   publications {
     create<MavenPublication>("maven") {
+      groupId = "org.jetbrains.projector-client"
+      artifactId = "projector-agent-initialization"
+      version = "-SNAPSHOT"
       from(components["java"])
+    }
+  }
+  repositories {
+    maven {
+      url = uri("https://packages.jetbrains.team/maven/p/prj/projector-client")
+      credentials {
+        username = usernameProp
+        password = passwordProp
+      }
     }
   }
 }
