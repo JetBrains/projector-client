c2afbd448ddc0402305df0e7e6b2b6b9f7dbf3ef
Antonov Andrey <Antonov-Andrey144@yandex.ru>
2021-08-30 13:41:43 +0300

    added tests for 18 ides

--- projector-server-core/src/intTest/kotlin/org/jetbrains/projector/intTest/idesTests/idesTests.kt
+++ projector-server-core/src/intTest/kotlin/org/jetbrains/projector/intTest/idesTests/idesTests.kt
@@ -26,18 +26,12 @@ package org.jetbrains.projector.intTest.idesTests
 import com.codeborne.selenide.Condition
 import com.codeborne.selenide.Selectors.byXpath
 import com.codeborne.selenide.Selenide.*
-import com.codeborne.selenide.WebDriverRunner.driver
 import com.codeborne.selenide.WebDriverRunner.getWebDriver
-import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.openqa.selenium.Keys
 import org.openqa.selenium.WebElement
 import org.openqa.selenium.interactions.Actions
-import org.openqa.selenium.interactions.touch.TouchActions
-import java.io.IOException
-import java.util.concurrent.TimeUnit
 import kotlin.test.Ignore
-
 
 class IdesTest
 {
@@ -63,13 +57,34 @@ class IdesTest
     Thread.sleep(200)
     elementBody.sendKeys(Keys.TAB)
     Thread.sleep(80)
-    elementBody.sendKeys(Keys.SPACE)      // Принимаем все лицензионные согл., жмём чтобы наши данные не отправлялись
+    elementBody.sendKeys(Keys.SPACE)      // Accapt Agreements
     if (ide != "pycharmcom2020.3.3") {
-      if (!(ide == "clion2020.3.2" || ide == "datagrip2020.3.2")) {
+      if (!(ide == "clion2020.3.2" || ide == "datagrip2020.3.2" || ide == "rider")) {
         `$`(byXpath("/html/body/canvas[7]")).should(Condition.appear)
         `$`(byXpath("/html/body/canvas[7]")).shouldBe(Condition.visible)
         Thread.sleep(500)
-        action.moveToElement(elementBody, -300, 640).click().build().perform()      // Выбираем тему по умолчанию
+        action.moveToElement(elementBody, -300, 640).click().build().perform()      // set default theme
+        `$`(byXpath("/html/body/canvas[5]")).should(Condition.disappear)
+        `$`(byXpath("/html/body/canvas[5]")).should(Condition.appear)
+      }
+      else if (ide == "rider")
+      {
+        `$`(byXpath("/html/body/canvas[8]")).should(Condition.appear)
+        `$`(byXpath("/html/body/canvas[8]")).shouldBe(Condition.visible)
+
+        action.moveToElement(elementBody, 270, 640).click().build().perform()
+        Thread.sleep(200)
+        action.moveToElement(elementBody, 270, 640).click().build().perform()
+        Thread.sleep(200)
+        action.moveToElement(elementBody, 270, 640).click().build().perform()
+        Thread.sleep(200)
+        action.moveToElement(elementBody, 270, 640).click().build().perform()
+        Thread.sleep(200)
+        action.moveToElement(elementBody, 270, 640).click().build().perform()
+        Thread.sleep(200)
+        action.moveToElement(elementBody, 270, 640).click().build().perform()
+        Thread.sleep(200)
+
         `$`(byXpath("/html/body/canvas[5]")).should(Condition.disappear)
         `$`(byXpath("/html/body/canvas[5]")).should(Condition.appear)
       }
@@ -81,25 +96,32 @@ class IdesTest
 
       if (ide != "pycharmcom2020.2") {
         Thread.sleep(100)
-        action.moveToElement(elementBody, -250, 10).click().build().perform()   // Жмём на бесплатную версию
+        action.moveToElement(elementBody, -250, 10).click().build().perform()   // click free trial
         Thread.sleep(80)
         pressTab(3, elementBody)
         if (ide != "datagrip2020.3.2")
           pressTab(1, elementBody)
-        elementBody.sendKeys(Keys.SPACE)      // выбираем и подтверждаем бесплатную версию
+        elementBody.sendKeys(Keys.SPACE)
 
-        `$`(byXpath("/html/body/canvas[5]")).should(Condition.disappear)
         if (ide == "clion2020.2" || ide == "goland2020.2.2") {
+          `$`(byXpath("/html/body/canvas[5]")).should(Condition.disappear)
           `$`(byXpath("/html/body/canvas[5]")).should(Condition.appear)
-          Thread.sleep(600)       // ждём, пока прогрузятся кнопки в появившемся окне
+          Thread.sleep(600)
           pressTab(7, elementBody)
-          elementBody.sendKeys(Keys.SPACE)      //соглашаемся со всякими настройками и запускаем программу
+          elementBody.sendKeys(Keys.SPACE)      //accept settings and run ide
+          Thread.sleep(80)
+          `$`(byXpath("/html/body/canvas[2]")).shouldBe(Condition.visible)
+        }
+        if (ide == "rider")
+        {
+          pressTab(3, elementBody)
+          elementBody.sendKeys(Keys.SPACE)      //accept settings and run ide
           Thread.sleep(80)
           `$`(byXpath("/html/body/canvas[2]")).shouldBe(Condition.visible)
         }
       }
     }
-    Thread.sleep(200)
+    Thread.sleep(300)
     close()
   }
 
@@ -119,27 +141,27 @@ class IdesTest
       Thread.sleep(200)
       elementBody.sendKeys(Keys.TAB)
       Thread.sleep(80)
-      elementBody.sendKeys(Keys.SPACE)      // Принимаем все лицензионные согл., жмём чтобы наши данные не отправлялись
+      elementBody.sendKeys(Keys.SPACE)      // Accapt Agreements
       `$`(byXpath("/html/body/canvas[5]")).should(Condition.visible)
-      action.moveToElement(`$`(byXpath("/html/body/canvas[5]")), 0, 30).click().build().perform()  // Жмём на бесплатную версию
+      action.moveToElement(`$`(byXpath("/html/body/canvas[5]")), 0, 30).click().build().perform()  // click free trial
       Thread.sleep(80)
       pressTab(3, elementBody)
       Thread.sleep(80)
-      elementBody.sendKeys(Keys.SPACE)      // выбираем и подтверждаем бесплатную версию
+      elementBody.sendKeys(Keys.SPACE)
       `$`(byXpath("/html/body/canvas[5]")).should(Condition.disappear)
       `$`(byXpath("/html/body/canvas[5]")).should(Condition.appear)
-      action.moveToElement(elementBody, -500, 670).click().build().perform()      // Выбираем тему по умолчанию
+      action.moveToElement(elementBody, -500, 670).click().build().perform()      // set default theme
       `$`(byXpath("/html/body/canvas[5]")).should(Condition.disappear)
     }
 
     if (ide == "datagrip2020.3.2") {
       Thread.sleep(1500)
-      action.moveToElement(`$`(byXpath("/html/body/canvas[4]")), -530, 315).click().build().perform()      // Выбираем тему по умолчанию
+      action.moveToElement(`$`(byXpath("/html/body/canvas[4]")), -530, 315).click().build().perform()      // set default theme
       `$`(byXpath("/html/body/canvas[5]")).should(Condition.disappear)
     }
     if (ide == "datagrip2019.3.4") {
       Thread.sleep(2000)
-      action.moveToElement(`$`(byXpath("/html/body/canvas[5]")), -330, 315).click().build().perform()      // Выбираем тему по умолчанию
+      action.moveToElement(`$`(byXpath("/html/body/canvas[5]")), -330, 315).click().build().perform()      // set default theme
       `$`(byXpath("/html/body/canvas[5]")).should(Condition.disappear)
     }
 
@@ -176,15 +198,15 @@ class IdesTest
     Thread.sleep(500)
     action.sendKeys(Keys.TAB).build().perform()
     Thread.sleep(80)
-    action.sendKeys(Keys.SPACE).build().perform()       // Принимаем все лицензионные согл., жмём чтобы наши данные не отправлялись
+    action.sendKeys(Keys.SPACE).build().perform()       // Accapt Agreements
     Thread.sleep(80)
     if (ide != "datagrip2019.3.4") {
       Thread.sleep(2000)
-      action.moveToElement(elementBody, -300, 640).click().build().perform()      // Выбираем тему по умолчанию
+      action.moveToElement(elementBody, -300, 640).click().build().perform()      // set default theme
       Thread.sleep(1200)
     }
     val elementCanvas: WebElement = `$`(byXpath("/html/body/canvas[8]"))
-    action.moveToElement(elementCanvas, 0, 30).click().build().perform()   // Жмём на бесплатную версию
+    action.moveToElement(elementCanvas, 0, 30).click().build().perform()   // click free trial
     Thread.sleep(400)
 
     if (ide == "datagrip2019.3.4") {
@@ -193,16 +215,16 @@ class IdesTest
       Thread.sleep(80)
     }
     pressTab(4, elementBody)
-    action.sendKeys(Keys.SPACE).build().perform()       // выбираем и подтверждаем бесплатную версию
+    action.sendKeys(Keys.SPACE).build().perform()
     Thread.sleep(80)
     pressTab(4, elementBody)
     Thread.sleep(80)
-    action.sendKeys(Keys.SPACE).build().perform()       // жмём продолжить
+    action.sendKeys(Keys.SPACE).build().perform()
     Thread.sleep(500)
     if (ide != "datagrip2019.3.4") {
       Thread.sleep(3500)
       pressTab(7, elementBody)
-      action.sendKeys(Keys.SPACE).build().perform()       //соглашаемся со всякими настройками и запускаем программу
+      action.sendKeys(Keys.SPACE).build().perform()       //accept settings and run ide
       Thread.sleep(1000)
     }
 
@@ -210,7 +232,7 @@ class IdesTest
     close()
   }
 
-  private fun openReadmeAndCopyTextForCLionLight(ide: String) {
+  private fun openReadmeForCLionLight(ide: String) {
     open("http://localhost:8887/projector/")
     val action = Actions(getWebDriver())
     val elementBody: WebElement = `$`(byXpath("//body"))
@@ -240,7 +262,7 @@ class IdesTest
     Thread.sleep(100)
   }
 
-  private fun openReadmeAndCopyTextForCLion03(ide: String) {
+  private fun openReadmeForCLion03(ide: String) {
     open("http://localhost:8887/projector/")
 
     val action = Actions(getWebDriver())
@@ -251,20 +273,28 @@ class IdesTest
     Thread.sleep(200)
     action.sendKeys(Keys.SPACE).build().perform()
     Thread.sleep(1000)
-
+    action.sendKeys(Keys.BACK_SPACE).build().perform()
+    Thread.sleep(1000)
     action.sendKeys("/home/projector-user/DemoProject/README.md").build().perform()
     Thread.sleep(400)
     elementBody.sendKeys(Keys.ENTER)
     `$`(byXpath("/html/body/iframe")).waitUntil(Condition.appear, 10000)
   }
 
-  private val ans = """# DemoProject
-This is a default sample project.
+  fun openReadmeForIdea() {
+    open("http://localhost:8887/projector/")
 
-Take a look at PJ logo:
+    val action = Actions(getWebDriver())
+    val elementBody: WebElement = `$`(byXpath("//body"))
 
-![PJ](src/pj.png)
-"""
+    `$`(byXpath("/html/body/canvas[8]")).waitUntil(Condition.appear, 10000)
+    action.moveToElement(`$`(byXpath("/html/body/canvas[8]")), 280, 1).click().build().perform()
+    Thread.sleep(200)
+    action.moveToElement(elementBody, -650, 240).doubleClick().build().perform()
+    `$`(byXpath("/html/body/iframe")).waitUntil(Condition.appear, 10000)
+
+    close()
+  }
 
   @Test
   fun cLionTest1() //CLion 2020.2
@@ -273,7 +303,7 @@ Take a look at PJ logo:
     //Runtime.getRuntime().exec("src/openIde.sh").waitFor(100, TimeUnit.SECONDS)
 
     accaptAgreementsAndSetSettings("clion2020.2")
-    openReadmeAndCopyTextForCLionLight("clion2020.2")
+    openReadmeForCLionLight("clion2020.2")
   }
 
   // ide freezes
@@ -289,21 +319,21 @@ Take a look at PJ logo:
     close()
 
     accaptAgreementsAndSetSettings1("clion2019.3.5")
-    openReadmeAndCopyTextForCLionLight("clion2019.3.5")
+    openReadmeForCLionLight("clion2019.3.5")
   }
 
   @Test
   fun cLionTest3()    //CLion 2020.3.2
   {
     accaptAgreementsAndSetSettings("clion2020.3.2")
-    openReadmeAndCopyTextForCLion03("clion2020.3.2")
+    openReadmeForCLion03("clion2020.3.2")
   }
 
   @Test
   fun goLand2()       // GoLand 2020.2.2
   {
     accaptAgreementsAndSetSettings("goland2020.2.2")
-    openReadmeAndCopyTextForCLionLight("goland2020.2.2")
+    openReadmeForCLionLight("goland2020.2.2")
   }
 
   // ide freezes
@@ -318,14 +348,14 @@ Take a look at PJ logo:
     action.moveToElement(elementBody, 205, 10).click().build().perform() //close import
     close()
     accaptAgreementsAndSetSettings1("goland2019.3.4")
-    //openReadmeAndCopyTextForCLion03("goland2019.3.4")   //todo: it doesn't work, if the problem with ide freezing will be solved, then you will need to write a function that opens readme.md
+    //openReadmeForCLion03("goland2019.3.4")   //todo: it doesn't work, if the problem with ide freezing will be solved, then you will need to write a function that opens readme.md
   }
 
   @Test
   fun goLand3()       //Goland 2020.3.2
   {
     accaptAgreementsAndSetSettings("clion2020.3.2")
-    openReadmeAndCopyTextForCLion03("goland2020.3.2")
+    openReadmeForCLion03("goland2020.3.2")
   }
 
   @Test
@@ -367,21 +397,21 @@ Take a look at PJ logo:
     close()
 
     accaptAgreementsAndSetSettings1("clion2019.3.5")
-    openReadmeAndCopyTextForCLionLight("phpstorm2020.2")
+    openReadmeForCLionLight("phpstorm2020.2")
   }
 
   @Test
   fun phpStorm2()     //PhpStorm 2020.2
   {
     accaptAgreementsAndSetSettings("clion2020.2")
-    openReadmeAndCopyTextForCLionLight("phpstorm2020.2")
+    openReadmeForCLionLight("phpstorm2020.2")
   }
 
   @Test
   fun phpStorm3()     //PhpStorm 2020.3.2
   {
     accaptAgreementsAndSetSettings("clion2020.3.2")
-    openReadmeAndCopyTextForCLion03("clion2020.3.2")
+    openReadmeForCLion03("clion2020.3.2")
   }
 
   //ide freezes
@@ -396,116 +426,181 @@ Take a look at PJ logo:
     action.moveToElement(elementBody, 205, 10).click().build().perform() //close import
     close()
     //accaptAgreementsAndSetSettings1("clion2019.3.5") // todo: it doesn't work, if the problem with ide freezing will be solved, then you will need to write a function that opens readme.md
-    //openReadmeAndCopyTextForCLionLight("phpstorm2020.2")
+    //openReadmeForCLionLight("phpstorm2020.2")
   }
 
   @Test
   fun pyCharmCommunity2()     //PyCharm Community 2020.2
   {
     accaptAgreementsAndSetSettings("pycharmcom2020.2")
-    openReadmeAndCopyTextForCLionLight("pycharmcom2020.2")
+    openReadmeForCLionLight("pycharmcom2020.2")
   }
 
   @Test
   fun pyCharmCommunity3()     //PyCharm Community 2020.3.3
   {
     accaptAgreementsAndSetSettings("pycharmcom2020.3.3")
-    openReadmeAndCopyTextForCLion03("clion2020.3.2")
+    openReadmeForCLion03("clion2020.3.2")
   }
 
+  //ide freezes
+  @Ignore
   @Test
   fun pyCharmProfessional19()      //PyCharm Professional 2019.3.4
   {
+    open("http://localhost:8887/")
+    val action = Actions(getWebDriver())
+    val elementBody: WebElement = `$`(byXpath("/html/body/canvas[8]"))
 
+    action.moveToElement(elementBody, 205, 10).click().build().perform() //close import
+    close()
+
+    accaptAgreementsAndSetSettings1("clion2019.3.5")
+    openReadmeForCLionLight("pycharmcom2020.2")
   }
 
   @Test
   fun pyCharmProfessional2()     //PyCharm Professional 2020.2
   {
-
+    accaptAgreementsAndSetSettings("clion2020.2")
+    openReadmeForCLionLight("pycharmcom2020.2")
   }
 
   @Test
   fun pyCharmProfessional3()     //PyCharm Professional 2020.3.3
   {
+    accaptAgreementsAndSetSettings("clion2020.3.2")
+    openReadmeForCLion03("clion2020.3.2")
+  }
 
+  @Test
+  fun pyCharmProfessional5()     //PyCharm Professional 2020.3.3
+  {
+    accaptAgreementsAndSetSettings("clion2020.3.2")
+    openReadmeForCLion03("clion2020.3.2")
   }
 
   @Test
   fun rubyMine()      //RubyMine 2020.3.2
   {
     accaptAgreementsAndSetSettings("clion2020.3.2")
-    openReadmeAndCopyTextForCLion03("clion2020.3.2")
+    openReadmeForCLion03("clion2020.3.2")
   }
 
   @Test
   fun webStorm()      //WebStorm 2020.3.2
   {
     accaptAgreementsAndSetSettings("clion2020.3.2")
-    openReadmeAndCopyTextForCLion03("clion2020.3.2")
+    openReadmeForCLion03("clion2020.3.2")
+  }
+
+  @Test
+  fun webStorm3()   //WebStorm 2020.3.3
+  {
+    accaptAgreementsAndSetSettings("clion2020.3.2")
+    openReadmeForCLion03("clion2020.3.2")
   }
 
   @Test
   fun rider()     //Rider 2020.3.3
   {
-    accaptAgreementsAndSetSettings("clion2020.3.2")
-    openReadmeAndCopyTextForCLion03("clion2020.3.2")
+    accaptAgreementsAndSetSettings("rider")
+    openReadmeForCLion03("clion2020.3.2")
   }
 
+  // failing to download from https://download.jetbrains.com/python/jetbrains-data-spell-212.443.21.tar.gz
+  @Ignore
   @Test
   fun dataSpell()   //DataSpell EAP 2021.1
   {
-    accaptAgreementsAndSetSettings("clion2020.3.2")
-    openReadmeAndCopyTextForCLion03("clion2020.3.2")
+
   }
 
+  //no readme.md file
+  @Ignore
   @Test
-  fun MPS()       //MPS 2020.3
+  fun mps()       //MPS 2020.3
   {
-    accaptAgreementsAndSetSettings("clion2020.3.2")
-    openReadmeAndCopyTextForCLion03("clion2020.3.2")
+    accaptAgreementsAndSetSettings("clion2020.3.2") //todo change this to close "import" menu
+    openReadmeForCLion03("clion2020.3.2")
   }
 
+  //ide freezes
+  @Ignore
   @Test
   fun ideaCommunity19()       //IntelliJ IDEA Community 2019.3.4
   {
-
+    //accaptAgreementsAndSetSettings("pycharmcom2020.3.3")// todo write "accaptAgreementsAndSetSettings" fun
+    Thread.sleep(4000)
+    openReadmeForIdea()
   }
 
   @Test
   fun ideaCommunity1()        //IntelliJ IDEA Community 2020.1.1
   {
-
+    accaptAgreementsAndSetSettings("pycharmcom2020.3.3")
+    Thread.sleep(4000)
+    openReadmeForIdea()
   }
 
   @Test
   fun ideaCommunity2()        //IntelliJ IDEA Community 2020.2
   {
-
+    accaptAgreementsAndSetSettings("pycharmcom2020.3.3")
+    Thread.sleep(4000)
+    openReadmeForIdea()
   }
 
   @Test
   fun ideaCommunity3()        //IntelliJ IDEA Community 2020.3.2
   {
-
+    accaptAgreementsAndSetSettings("pycharmcom2020.3.3")
+    Thread.sleep(4000)
+    openReadmeForIdea()
   }
 
   @Test
+  fun ideaCommunity4()        //IntelliJ IDEA Community 2020.3.4
+  {
+    accaptAgreementsAndSetSettings("pycharmcom2020.3.3")
+    Thread.sleep(4000)
+    openReadmeForIdea()
+  }
+
+  //ide freezes
+  @Ignore
+  @Test
   fun ideaUltimate19()       //IntelliJ IDEA Ultimate 2019.3.4
   {
+    open("http://localhost:8887/")
+    val action = Actions(getWebDriver())
+    val elementBody: WebElement = `$`(byXpath("/html/body/canvas[8]"))
 
+    action.moveToElement(elementBody, 205, 10).click().build().perform() //close import
+    close()
+
+    accaptAgreementsAndSetSettings1("clion2019.3.5")
+    openReadmeForCLionLight("clion2019.3.5")
   }
 
   @Test
   fun ideaUltimate2()        //IntelliJ IDEA Ultimate 2020.2
   {
-
+    accaptAgreementsAndSetSettings("clion2020.2")
+    openReadmeForCLionLight("pycharmcom2020.2")
   }
 
   @Test
   fun ideaUltimate3()        //IntelliJ IDEA Ultimate 2020.3.2
   {
     accaptAgreementsAndSetSettings("clion2020.3.2")
-    openReadmeAndCopyTextForCLion03("clion2020.3.2")
+    openReadmeForCLion03("clion2020.3.2")
+  }
+
+  @Test
+  fun ideaUltimate4()        //IntelliJ IDEA Ultimate 2020.3.4
+  {
+    accaptAgreementsAndSetSettings("clion2020.3.2")
+    openReadmeForCLion03("clion2020.3.2")
   }
 }
