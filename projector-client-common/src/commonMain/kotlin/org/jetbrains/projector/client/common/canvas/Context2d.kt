21f2a4d7e058699e9bee979b7ca2c90605d89b12
SerVB <servbul@yandex.ru>
2021-11-25 17:47:40 +0300

    PRJ-199 Optimization: avoid flushing the buffer if it's unchanged

--- projector-client-common/src/commonMain/kotlin/org/jetbrains/projector/client/common/canvas/Context2d.kt
+++ projector-client-common/src/commonMain/kotlin/org/jetbrains/projector/client/common/canvas/Context2d.kt
@@ -28,27 +28,11 @@ import org.jetbrains.projector.client.common.canvas.PaintColor.Gradient
 import org.jetbrains.projector.common.protocol.data.PathSegment
 import org.jetbrains.projector.common.protocol.data.Point
 
-interface Context2d {
-  fun clearRect(x: Double, y: Double, w: Double, h: Double)
-  fun drawImage(imageSource: ImageSource, x: Double, y: Double)
-  fun drawImage(imageSource: ImageSource, x: Double, y: Double, dw: Double, dh: Double)
-  fun drawImage(
-    imageSource: ImageSource,
-    sx: Double,
-    sy: Double,
-    sw: Double,
-    sh: Double,
-    dx: Double,
-    dy: Double,
-    dw: Double,
-    dh: Double,
-  )
+/** These operations don't change the bitmap. */
+interface Context2dStateOperations {
 
   fun beginPath()
   fun closePath()
-  fun stroke()
-  fun fill(fillRule: FillRule = FillRule.NONZERO)
-  fun fillRect(x: Double, y: Double, w: Double, h: Double)
   fun moveTo(x: Double, y: Double)
   fun moveBySegments(segments: List<PathSegment>)
   fun moveByPoints(points: List<Point>)
@@ -66,38 +50,69 @@ interface Context2d {
     anticlockwise: Boolean = false,
   )
 
+  fun bezierCurveTo(cp1x: Double, cp1y: Double, cp2x: Double, cp2y: Double, x: Double, y: Double)
+  fun quadraticCurveTo(cpx: Double, cpy: Double, x: Double, y: Double)
+
   fun save()
   fun restore()
+
   fun setFillStyle(color: PaintColor?)
   fun setStrokeStyle(color: PaintColor?)
   fun setGlobalAlpha(alpha: Double)
-  fun setGlobalCompositeOperation(type: CompositeOperationType)
+  fun setGlobalCompositeOperation(type: Context2d.CompositeOperationType)
   fun setFont(f: String)
   fun setLineWidth(lineWidth: Double)
-  fun strokeRect(x: Double, y: Double, w: Double, h: Double)
-  fun strokeText(text: String, x: Double, y: Double)
-  fun fillText(text: String, x: Double, y: Double)
+  fun setLineJoin(lineJoin: Context2d.LineJoin)
+  fun setMiterLimit(limit: Double)
+  fun setLineCap(lineCap: Context2d.LineCap)
+  fun setTextBaseline(baseline: Context2d.TextBaseline)
+  fun setTextAlign(align: Context2d.TextAlign)
+  fun setTransform(m11: Double, m12: Double, m21: Double, m22: Double, dx: Double, dy: Double)
+  fun setLineDash(lineDash: DoubleArray)
+  fun setLineDashOffset(offset: Double)
+
   fun scale(x: Double, y: Double)
   fun rotate(angle: Double)
   fun translate(x: Double, y: Double)
   fun transform(m11: Double, m12: Double, m21: Double, m22: Double, dx: Double, dy: Double)
-  fun bezierCurveTo(cp1x: Double, cp1y: Double, cp2x: Double, cp2y: Double, x: Double, y: Double)
-  fun quadraticCurveTo(cpx: Double, cpy: Double, x: Double, y: Double)
-  fun setLineJoin(lineJoin: LineJoin)
-  fun setMiterLimit(limit: Double)
-  fun setLineCap(lineCap: LineCap)
-  fun setTextBaseline(baseline: TextBaseline)
-  fun setTextAlign(align: TextAlign)
-  fun setTransform(m11: Double, m12: Double, m21: Double, m22: Double, dx: Double, dy: Double)
-  fun setLineDash(lineDash: DoubleArray)
-  fun setLineDashOffset(offset: Double)
+
   fun measureText(str: String): Point
 
-  fun clip(fillRule: FillRule? = null)
+  fun clip(fillRule: Context2d.FillRule? = null)
 
   fun createLinearGradient(x0: Double, y0: Double, x1: Double, y1: Double): Gradient
 
-  fun getTransform(): Matrix
+  fun getTransform(): Context2d.Matrix
+}
+
+/** These operations change the bitmap. */
+interface Context2dDrawOperations {
+
+  fun clearRect(x: Double, y: Double, w: Double, h: Double)
+  fun drawImage(imageSource: ImageSource, x: Double, y: Double)
+  fun drawImage(imageSource: ImageSource, x: Double, y: Double, dw: Double, dh: Double)
+  fun drawImage(
+    imageSource: ImageSource,
+    sx: Double,
+    sy: Double,
+    sw: Double,
+    sh: Double,
+    dx: Double,
+    dy: Double,
+    dw: Double,
+    dh: Double,
+  )
+
+  fun stroke()
+  fun fill(fillRule: Context2d.FillRule = Context2d.FillRule.NONZERO)
+  fun fillRect(x: Double, y: Double, w: Double, h: Double)
+
+  fun strokeRect(x: Double, y: Double, w: Double, h: Double)
+  fun strokeText(text: String, x: Double, y: Double)
+  fun fillText(text: String, x: Double, y: Double)
+}
+
+interface Context2d : Context2dStateOperations, Context2dDrawOperations {
 
   enum class LineJoin {
     BEVEL, MITER, ROUND
--- projector-client-common/src/commonMain/kotlin/org/jetbrains/projector/client/common/canvas/buffering/DoubleBufferedRenderingSurface.kt
+++ projector-client-common/src/commonMain/kotlin/org/jetbrains/projector/client/common/canvas/buffering/DoubleBufferedRenderingSurface.kt
@@ -25,13 +25,15 @@ package org.jetbrains.projector.client.common.canvas.buffering
 
 import org.jetbrains.projector.client.common.canvas.Canvas
 import org.jetbrains.projector.client.common.canvas.CanvasFactory
+import org.jetbrains.projector.client.common.canvas.Context2d
+import org.jetbrains.projector.client.common.canvas.Context2dStateOperations
 import org.jetbrains.projector.client.common.canvas.Extensions.resizeSavingImage
 
 class DoubleBufferedRenderingSurface(private val bufferCanvasFactory: CanvasFactory, private val target: Canvas) : RenderingSurface {
 
   override var scalingRatio: Double = 1.0
 
-  private val buffer = createBuffer()
+  private val buffer = Buffer(bufferCanvasFactory, width = target.width, height = target.height)
 
   override val canvas: Canvas
     get() = buffer
@@ -44,14 +46,75 @@ class DoubleBufferedRenderingSurface(private val bufferCanvasFactory: CanvasFact
   override fun flush() {
     if (buffer.width == 0 || buffer.height == 0) return
 
-    target.context2d.drawImage(buffer.imageSource, 0.0, 0.0)
+    // optimization: flush only if the buffer is changed
+    if (buffer.changed) {
+      buffer.resetChanged()
+      target.context2d.drawImage(buffer.imageSource, 0.0, 0.0)
+    }
   }
 
-  private fun createBuffer(): Canvas {
-    return bufferCanvasFactory.create()
-      .apply {
-        width = target.width
-        height = target.height
+  /** A canvas that knows if there have been operations that could modify its bitmap. */
+  private class Buffer(bufferCanvasFactory: CanvasFactory, width: Int, height: Int) : Canvas {
+
+    private val delegate = bufferCanvasFactory.create()
+      .also {
+        it.width = width
+        it.height = height
       }
+
+    override val imageSource by delegate::imageSource
+    override fun takeSnapshot() = delegate.takeSnapshot()
+
+    var changed = false
+      private set
+
+    fun resetChanged() {
+      changed = false
+    }
+
+    private fun change() {
+      changed = true
+    }
+
+    private inline fun withChange(operation: Canvas.() -> Unit) {
+      change()
+      delegate.operation()
+    }
+
+    override var width
+      get() = delegate.width
+      set(value) = withChange { width = value }
+
+    override var height
+      get() = delegate.height
+      set(value) = withChange { height = value }
+
+    // todo: rewrite boilerplate code to decorators of Context2dDrawOperations when supported: https://youtrack.jetbrains.com/issue/KT-49904
+    override val context2d: Context2d = object : Context2dStateOperations by delegate.context2d, Context2d {
+
+      private inline fun withChange2d(operation: Context2d.() -> Unit) {
+        change()
+        delegate.context2d.operation()
+      }
+
+      override fun clearRect(x: Double, y: Double, w: Double, h: Double) = withChange2d { clearRect(x = x, y = y, w = w, h = h) }
+      override fun drawImage(imageSource: Canvas.ImageSource, x: Double, y: Double) = withChange2d { drawImage(imageSource, x = x, y = y) }
+      override fun drawImage(imageSource: Canvas.ImageSource, x: Double, y: Double, dw: Double, dh: Double) =
+        withChange2d { drawImage(imageSource, x = x, y = y, dw = dw, dh = dh) }
+
+      override fun drawImage(
+        imageSource: Canvas.ImageSource,
+        sx: Double, sy: Double, sw: Double, sh: Double,
+        dx: Double, dy: Double, dw: Double, dh: Double,
+      ) =
+        withChange2d { drawImage(imageSource, sx = sx, sy = sy, sw = sw, sh = sh, dx = dx, dy = dy, dw = dw, dh = dh) }
+
+      override fun stroke() = withChange2d { stroke() }
+      override fun fill(fillRule: Context2d.FillRule) = withChange2d { fill(fillRule) }
+      override fun fillRect(x: Double, y: Double, w: Double, h: Double) = withChange2d { fillRect(x = x, y = y, w = w, h = h) }
+      override fun strokeRect(x: Double, y: Double, w: Double, h: Double) = withChange2d { strokeRect(x = x, y = y, w = w, h = h) }
+      override fun strokeText(text: String, x: Double, y: Double) = withChange2d { strokeText(text = text, x = x, y = y) }
+      override fun fillText(text: String, x: Double, y: Double) = withChange2d { fillText(text = text, x = x, y = y) }
+    }
   }
 }
