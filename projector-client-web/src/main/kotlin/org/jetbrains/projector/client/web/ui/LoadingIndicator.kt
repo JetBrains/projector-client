d3f64a5ade47e14799e79da6870244b446b5145b
SerVB <servbul@yandex.ru>
2022-02-24 19:50:59 +0100

    Rewrite CSS to Kotlin DSL for consistency

--- projector-client-web/src/main/kotlin/org/jetbrains/projector/client/web/ui/LoadingIndicator.kt
+++ projector-client-web/src/main/kotlin/org/jetbrains/projector/client/web/ui/LoadingIndicator.kt
@@ -24,12 +24,67 @@
 package org.jetbrains.projector.client.web.ui
 
 import androidx.compose.runtime.Composable
+import org.jetbrains.compose.web.css.*
+import org.jetbrains.compose.web.css.selectors.Nth.Companion.Functional
 import org.jetbrains.compose.web.dom.Div
+
+private const val CHILDREN_COUNT = 12
+
+private object LoadingIndicatorStyleSheet : StyleSheet() {
+
+  private val ldsSpinnerFrames by keyframes {
+    0.percent {
+      opacity(1)
+    }
+
+    100.percent {
+      opacity(0)
+    }
+  }
+
+  val ldsSpinner by style {
+    color(Color.white)
+    display(DisplayStyle.InlineBlock)
+    position(Position.Relative)
+    width(80.px)
+    height(80.px)
+
+    type("div") style {
+      property("transform-origin", "40px 40px")
+      animation(ldsSpinnerFrames) {
+        duration(1.2.s)
+        timingFunction(AnimationTimingFunction.Linear)
+        iterationCount(null)  // null is infinite
+      }
+    }
+
+    type("div") + after style {
+      property("content", """" """")
+      display(DisplayStyle.Block)
+      position(Position.Absolute)
+      top(3.px)
+      left(37.px)
+      width(6.px)
+      height(18.px)
+      borderRadius(20.percent)
+      background("#fff")
+    }
+
+    (1..CHILDREN_COUNT).forEach { n ->
+      type("div") + nthChild(Functional(b = n)) style {
+        property("transform", "rotate(${(n - 1) * 30}deg)")
+        property("animation-delay", "${(-1.2 + n * 0.1)}s")
+      }
+    }
+  }
+}
 
 @Composable
 fun LoadingIndicator() {
-  Div({ classes("lds-spinner") }) {
-    repeat(12) {
+  Style(LoadingIndicatorStyleSheet)
+
+  Div({ classes(LoadingIndicatorStyleSheet.ldsSpinner) }) {
+    repeat(CHILDREN_COUNT) {
       Div()
     }
   }
